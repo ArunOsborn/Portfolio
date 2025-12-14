f469db186983211f4002ed93b693c91542d80b92
Arun Osborn <arunosborn@gmail.com>
2025-12-14 15:18:08 +0000

    feat: home page put inside a mat-card and other small style changes

--- frontend/portfolio/src/app/home-page/home-page.ts
+++ frontend/portfolio/src/app/home-page/home-page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { MatTooltipModule } from "@angular/material/tooltip";
 
 @Component({
-  selector: 'app-home-page',
-  imports: [MatButtonModule],
-  templateUrl: './home-page.html',
-  styleUrl: './home-page.scss',
+	selector: 'app-home-page',
+	imports: [MatButtonModule, MatCardModule, MatTooltipModule],
+	templateUrl: './home-page.html',
+	styleUrl: './home-page.scss',
 })
 export class HomePage {
-
+	
 }
